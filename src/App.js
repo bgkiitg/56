cf5a1774e753e9496bf9d7b95f069ece54dd8621
Benny George Kenkireth <ben@iitg.ac.in>
2020-07-05 08:00:08 +0530

    Added the Validation of Bids to Game.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { Client } from 'boardgame.io/react';
 import { fiftysix } from './Game';
+import { fiftysixBoard} from './Board';
 
-const App = Client({ game: fiftysix });
+const App = Client({ 
+	game: fiftysix,
+	board:fiftysixBoard});
 
 export default App;
--- src/Game.js
+++ src/Game.js
@@ -3,62 +3,95 @@ import { INVALID_MOVE } from 'boardgame.io/core';
 export const fiftysix = {
   setup: () => ({ 
   	//cells: Array().fill(null),
+  	bids: [],
   	points: [12,12],
 	gamesPlayed: 0,
 	nextPlayer: 0,
-	numOfPlayers: 6,
-	hands: getRandomHand(6),
-	bidValue: '',
-	trump: '',
-	double: '',
-	doublerId: '',
-	redouble: '',
-	redoublerId: '',
-	bidderId: '',
-	bids: [],
+	numPlayers: 6,
+	//hands: getRandomHand(6),
+	bid: {
+		bidder: '',
+		value: '',
+		trump: '',
+		doubler: '',
+		redoubler: '',
+		bidCount: 0
+	},
 	disallowedBidders: [],
 	cardsPlayed: []
   }),
 
+  turn: {
+    moveLimit: 1,
+  },
+
   moves: {
   
 	bidTrump:  (G,ctx,bidString) => {
-		if(isValidBid(G,ctx,bidString)){
-			let bidValue = Number(bidString.slice(0,2))
-			let trump=bidString[2]
-			let double=Number(bidString[4])
-			let redouble=Number(bidString[5])
-			let pass=Number(bidString[6])
-			let plus=Number(bidString[7])
-			let plusValue=Number(bidString[8])
-			let bidderId=bidString[9] 
-
-			if (pass !== 0){
-				if(plus === 1){
-					G.bidValue = G.bidValue + plusValue
-					G.trump = trump
-					G.bidderId =bidderId
-					G.double =''
-					G.doublerId=''
-				}
-				if (double === 1){
-					G.double =1
-					G.doublerId=bidderId
-				}
-				if(redouble === 1){
-					G.redouble =1
-					G.redoublerId =bidderId
-				}
-				if(bidValue >28){
-					G.bidValue = bidValue
-					G.trump = trump
-					G.double=''
-					G.doublerId=''
-				}
-			}
+		let bidType = identifyBid(G,ctx,bidString)
+		console.log(bidString, bidType)
+		if(bidType >0){
+			G.bid.bidCount++
 			G.bids.push(bidString)
-		}else
-			return INVALID_MOVE
+		}
+
+		//Rework to incorporate the starting player
+		
+		switch(identifyBid(G,ctx,bidString)) {
+			case 0:
+				return INVALID_MOVE
+			case 1:
+				break;
+			case 2:
+				G.bid.doubler = ctx.currentPlayer
+				break;
+			case 3:
+				G.bid.redoubler = ctx.currentPlayer
+				break;
+			case 4:
+				G.bid.bidder = ctx.currentPlayer
+				G.bid.value = Number(bidString.slice(0,2))
+				G.bid.trump = bidString[2]
+				G.bid.doubler =''
+				G.bid.redoubler= ''
+				break;
+			case 5:
+				G.bid.bidder = ctx.currentPlayer
+				G.bid.value = Number(bidString[1])+G.bid.value
+				G.bid.trump = bidString[2]
+				G.bid.doubler =''
+				G.bid.redoubler= ''
+				break;
+			case 6:
+				G.bid.bidder = ctx.currentPlayer
+				G.bid.value = Number(bidString.slice(1,3))
+				G.bid.trump = bidString[0]
+				G.bid.doubler =''
+				G.bid.redoubler= ''
+				break;
+			case 7:
+				G.bid.bidder = ctx.currentPlayer
+				G.bid.value = Number(bidString.slice(0,2))
+				G.bid.trump = bidString.slice(2,4)
+				G.bid.doubler =''
+				G.bid.redoubler= ''
+				break;
+			case 8:
+				G.bid.bidder = ctx.currentPlayer
+				G.bid.value = Number(bidString.slice(0,2))
+				G.bid.trump = bidString.slice(2,4)
+				G.bid.doubler =''
+				G.bid.redoubler= ''
+				break;
+			default :
+				return INVALID_MOVE
+
+		}
+
+		if(G.bid.bidCount ===  G.numPlayers + 80000){
+			setUpDisallowedBidders(G,ctx)
+		}
+
 	},
     
 	playCard:  (G,ctx,cardPlayed) => {
@@ -73,86 +106,116 @@ export const fiftysix = {
   }
 };
 
-function isValidBid(G,ctx,bidString){
 
-	if(G.redouble === '1')
-		return false;
-
-	if(bidString.length !==  10)
-		return false;
-
-	let bidValue = Number(bidString.slice(0,2))
-	let trump=bidString[2]
-	let reverse=Number(bidString[3])
-	let double=Number(bidString[4])
-	let redouble=Number(bidString[5])
-	let pass=Number(bidString[6])
-	let plus=Number(bidString[7])
-	let plusValue=Number(bidString[8])
-	let bidderId=bidString[9] 
-
-	let bidderOkay = 1;
-	for ( let i=0; i<G.disallowedBidders.length; i++){
-		if(G.disallowedBidders[i] === bidderId){
-			bidderOkay = 0
-			break; 
+//DEbug Thus
+function setUpDisallowedBidders(G,ctx){
+	let evenTeamBid = 0
+	for(let i =1; i <G.numPlayers; i+2){
+		if(G.bids[i] !== 'Pass'){
+			evenTeamBid = 1
+			break;
 		}
-		
 	}
 
-	if ((bidValue>0  && bidValue<28) || bidValue>56)
-		return false;
 
-	if ((trump !== 'N') && (trump !=='O') && (trump !=='C') && (trump !=='D') && (trump !=='H') && (trump !=='S'))
-		return false;
+	let oddTeamBid = 0
+	for(let i =2; i <G.numPlayers; i+2){
+		if(G.bids[i] !== 'Pass'){
+			oddTeamBid = 1
+			break;
+		}
+	}
+	if (evenTeamBid === 0)
+		G.disallowedBidders = (ctx.currentPlayer%2 === 1) ? [1,3,5,7] : [2,4,6,8]
 
-	if (reverse+double+redouble+pass+plus >1)
-		return false;
-
-	if (plusValue>8) 
-		return false;
-
-	if (bidderId <0 || bidderId >= G.numOfPlayers)
-		return false; 
-
-
-	// Is the bid either Staight or reverse?
-
-	if ((bidValue>0) && (double+redouble+pass+plus+plusValue) === 0  && (G.bidValue < bidValue )  && bidderOkay === 1 )
-		return  true ;
-
-	//Is the bid a double?
-
-	let okayToDouble =((G.bidderId -bidderId)%2 === 1) && (double === 1)
-
-	if(bidValue === 0 && trump === 'U' && okayToDouble && (reverse+double+redouble+pass+plus+plusValue) === 1 && G.bidValue !== ''  )
-		return true ;
-
-	//Is the bid a redouble?
-	let okayToRedouble =((G.doublerId -bidderId)%2 === 1) && (double === 1) && G.double === 1
-
-	if(bidValue === 0 && trump === 'U' && okayToRedouble === 1 && (reverse+double+redouble+pass+plus+plusValue) === 1 )
-		return true;
-
-	//Is the bid a pass?
-	if(bidValue === 0 && trump === 'U' && pass === 1  && (reverse+double+redouble+pass+plus+plusValue) === 1 && G.bidValue !== '')
-		return true;
-
-
-	//Is the bid a plus bid?
-
-	let isTrumpBid = ( trump === 'C' || trump === 'D'|| trump ===' H'|| trump === 'S')
-	if (bidValue === 0 && isTrumpBid && plus === 1 && plusValue >0 && bidderOkay === 1 &&  reverse+double+redouble+pass === 0  && (G.bidValue + plusValue) < 57 )
-		return true;
-
-	return true;
+	let temp = []
+	if (oddTeamBid === 0){
+	
+		let tempPlayer = (ctx.currentPlayer+3)%G.numPlayers
+		for(let i = 1; i< G.numPlayers; i+2){
+			temp.push(tempPlayer)
+			tempPlayer = tempPlayer+ 2
+		}
+	}
+	G.disallowedBidders = G.disallowedBidders.concat(temp)
 }
 
+function identifyBid(G,ctx,bidString){
+	// The return values reflect the following
+	// 0 : Invalid Bid
+	// 1 : Pass
+	// 2 : Double
+	// 3 : Redouble
+	// 4 : Straight Bid 
+	// 5 : Plus Bid
+	// 6 : Reverse Bid
+	// 7 : NS
+	// 8 : NT
+	//No valid bids after redoubling
+	if(G.bid.redoubler !== '')
+		return 0
+
+	console.log(bidString)
+	switch(bidString) {
+		case "Pass":
+			console.log("Pass")
+			if(G.bid.bidder === '')
+				return 0
+			else
+				return 1
+		case "Double":
+			console.log("Double")
+			if((G.bid.bidder === '')  || (Math.abs(G.bid.bidder - ctx.currentPlayer))%2 === 0)
+				return 0
+			else 
+				return 2
+
+		case "Redouble":
+			console.log("Redouble")
+			if((G.bid.doubler === '') || (Math.abs(G.bid.doubler - ctx.currentPlayer))%2 ===0)
+				return 0
+			else 
+				return 3
+
+		default :
+			switch(bidString.length){
+				case 3:
+					console.log("Straight Reverse or Plus")
+					if(bidString[0]==='C' || bidString[0]==='D' || bidString[0]==='H' ||bidString[0]==='S' ){
+
+						if(Number(bidString.slice(1,3)) > G.bid.value && Number(bidString.slice(1,3)) <57)	return 6
+					}
+					if(bidString[0] === '+'){
+						let x= Number(bidString[1])
+						if( x >0 && x <9 && G.bid.value + x < 57){ return 5} else {return 0}
+					}
+					if(bidString[2]==='C' || bidString[2]==='D' || bidString[2]==='H' ||bidString[2]==='S' ){
+						if(Number(bidString.slice(0,2)) > G.bid.value && Number(bidString.slice(0,2)) <57)	return 4
+					}
+					return 0
+				case 4:
+					if(bidString.slice(2,4) === 'NS'){
+						if(Number(bidString.slice(0,2)) > G.bid.value && Number(bidString.slice(0,2)) <57)	return 7
+					}
+					if(bidString.slice(2,4) === 'NT'){
+						if(Number(bidString.slice(0,2)) > G.bid.value && Number(bidString.slice(0,2)) <57)	return 8
+					}
+					break ;
+				default:
+					return 0
+
+			}	
+	}
+}
+
+
 function isValidPlay(G,ctx,cardPlayed){
+
 	return true;
 }
 
 function findNextPlayer(G,ctx){
+	getRandomHand()
 	return 0;
 
 }
